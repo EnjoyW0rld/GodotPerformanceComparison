5717b0a79e5149e90b063e21e7653f33d3353148
EnjoyW0rld <denisavatar@hotmail.com>
2023-03-27 22:25:06 +0200

    Added queen checking algorithm
    Not yet fully tested

--- GameBoard.cs
+++ GameBoard.cs
@@ -12,8 +12,12 @@ public class GameBoard : Node
     [Export] private PackedScene tile;
     [Export] private PackedScene pawn;
     [Export] private int size;
+
+    public int BoardSize { get { return size; } }
+
     private int[,] _placedPawns;
     private Pawn[] pawns;
+
     private Vector2 scale = Vector2.Zero;
     private Vector2 basePos = Vector2.Zero;
 
@@ -46,6 +50,12 @@ public class GameBoard : Node
                 AddChild(newTile);
             }
         }
+
+        //_placedPawns[0, 1] = 1;
+        //_placedPawns[1, 3] = 1;
+        //_placedPawns[2, 0] = 1;
+        //_placedPawns[3, 2] = 1;
+        //GD.Print(IsSolved());
         InstantiatePawns();
 
     }
@@ -58,18 +68,98 @@ public class GameBoard : Node
             Pawn pawnScript = newPawn as Pawn;
             pawns[i] = pawnScript;
             pawnScript.Initialize(this, i, GetPosAt(0, i));
+            _placedPawns[0, i] = 1;
 
         }
+    }
+
+    private void MovePawn()
+    {
+        for (int i = 0; i < size; i++)
+        {
+
+
+            //pawns[0].Position = GetPosAt(0, i);
+        }
+        //pawns[0].Move();
     }
 
     public override void _Process(float delta)
     {
         //base._Process(delta);
-
+        if (Input.IsKeyPressed((int)KeyList.G))
+        {
+            GD.Print(IsSolved());
+        }
     }
 
     private Vector2 GetPosAt(int x, int y)
     {
         return basePos + new Vector2(scale.x * x, scale.y * y);
+    }
+
+    private bool IsSolved()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            int[] gridPos = pawns[i].GridPos;
+            if (CheckVertical(gridPos)
+                && CheckDiagonal(gridPos)
+                && CheckDiagonal(gridPos, 1)) continue;
+
+            return false;
+        }
+        return true;
+    }
+    /// <summary>
+    /// Put 1 in yOffset to mirror the check diagonal
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <param name="yOffset"></param>
+    /// <returns></returns>
+    private bool CheckDiagonal(int[] pos, int yOffset = -1)
+    {
+        int[] currentPos = pos;
+        currentPos[0]--;
+        currentPos[1] += yOffset;
+
+        while (currentPos[0] >= 0 && currentPos[1] >= 0)
+        {
+            if (_placedPawns[currentPos[0], currentPos[1]] != 0)
+            {
+                return false;
+            }
+            currentPos[0]--;
+            currentPos[1] += yOffset;
+        }
+
+        currentPos = pos;
+        currentPos[0]++;
+        currentPos[1] -= yOffset;
+
+        while (currentPos[0] < size && currentPos[1] < size)
+        {
+            if (_placedPawns[currentPos[0], currentPos[1]] != 0)
+            {
+                return false;
+            }
+            currentPos[0]++;
+            currentPos[1] -= yOffset;
+        }
+
+        return true;
+    }
+    private bool CheckVertical(int[] pos)
+    {
+        bool foundOrigin = false;
+        for (int y = 0; y < size; y++)
+        {
+            if (_placedPawns[pos[0], y] != 0)
+            {
+                if (foundOrigin) return false;
+                else foundOrigin = true;
+            }
+        }
+        return true;
     }
 }
--- Pawn.cs
+++ Pawn.cs
@@ -6,26 +6,32 @@ public class Pawn : Sprite
     private int id;
     private Pawn _childPawn;
     private GameBoard _board;
+    int[] pos = new int[2];
+    public int[] GridPos { get { return pos; } }
+
 
     public override void _Ready()
     {
-        
+
     }
 
-    public void Initialize(GameBoard board,int id, Vector2 pos)
+    public void Initialize(GameBoard board, int id, Vector2 pos)
     {
         this.id = id;
         this._board = board;
         Position = pos;
     }
+
     public void Move()
     {
+        for (int i = 0; i < _board.BoardSize; i++)
+        {
 
+        }
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
 }
